52f60da6e179873b290649c869ef0da2c61fbe92
Guilherme Ribeiro <desaribeiro.guilherme@gmail.com>
2022-07-19 14:52:30 -0300

    [refactor] extracting expression to variable

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -13,6 +13,8 @@ public class GreetingController {
 	
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+		long id = counter.incrementAndGet();
+		String content = String.format(template, name);
+		return new Greeting(id, content);
 	}
 }
